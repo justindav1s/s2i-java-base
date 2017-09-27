11a366b67a8c5d079ae55bbfafb2e71dbce470e0
justindav1s <justin.davis@ba.com>
2017-09-27 11:33:03 +0200

    fixes

---
+++ bin/ocp_deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+USER=justin
+PROJECT=test
+IMAGE=s2i-java-base
+REGISTRY_HOST=docker-registry-default.apps.192.168.140.152.xip.io
+
+oc login -u $USER
+
+oc delete project $PROJECT
+sleep 20
+oc new-project $PROJECT
+
+docker build -t $IMAGE ..
+docker tag $IMAGE $REGISTRY_HOST/$PROJECT/$IMAGE
+
+TOKEN=`oc whoami -t`
+
+docker login -p $TOKEN -u $USER $REGISTRY_HOST
+
+docker push $REGISTRY_HOST/$PROJECT/$IMAGE
+
+oc new-app $PROJECT/$IMAGE~https://github.com/justindav1s/simple-java-service.git
+
